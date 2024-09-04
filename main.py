139db8daea4cf9b965af9e42d8a44f898bc7bdcd
F1rePh0enix <jules.ponce@eleves.enpc.fr>
2024-09-04 15:19:06 +0200

    ask me a question is up

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import request
 from src.utils import ask_question_to_pdf
 
 app = Flask(__name__)
+conversation = []
 
 # @app.route("/")
 # def hello_world():
@@ -18,5 +19,13 @@ def home():
 @app.route("/prompt", methods=["POST"])
 def bot_prompt():
     if request.method == "POST":
-        response = ask_question_to_pdf.gpt3_completion(request.form["prompt"])
+        response = ask_question_to_pdf.gpt3_completion(
+            request.form["prompt"], chatlog=conversation)
+        return {"answer": response}
+
+
+@app.route("/question", methods=["GET"])
+def bot_submit():
+    if request.method == "GET":
+        response = ask_question_to_pdf.gpt3_question(chatlog=conversation)
         return {"answer": response}
